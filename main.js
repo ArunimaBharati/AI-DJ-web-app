d90825da90adedff181a43718f343ae6055656fe
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2022-05-14 18:36:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristY=0;
+rightWristX=0;
 
 function setup()
 {
@@ -6,6 +10,28 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is intilised');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY"+rightWristY);
+    }
 }
 
 function draw()
@@ -21,4 +47,8 @@ function preload()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+
 }
+
